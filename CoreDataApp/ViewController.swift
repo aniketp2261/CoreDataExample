8c16e0e28aea55be43f870eb2b0029bb93a1f1c1
Aniket Patil <aniketp2261@gmail.com>
2023-01-23 21:55:40 +0530

    Initial Commit

---
+++ CoreDataApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataApp
+//
+//  Created by Aniket Patil on 23/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
